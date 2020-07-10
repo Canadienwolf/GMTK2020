5f4d08312e7c464eacb69f238a2f9caa328fcedf
Frank <canadienwolf@gmail.com>
2020-07-11 00:35:28 +0200

    Auto stash before merge of "master" and "origin/master"

---
+++ GMTK2020/Assets/_Scripts/MeteorSpawner.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class MeteorSpawner : MonoBehaviour
+{
+    public GameObject meteor;
+    
+    //---------------------------------------------------------------------------------------------------------------------------------
+    
+    private float _min = 0.1f;
+    private float _max = 0.9f;
+    
+    private float _currentTime;
+    private float _spawnTime;
+
+    private Vector3 _final;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        _currentTime += Time.deltaTime;
+
+        if (_currentTime >= _spawnTime)
+        {
+            SpawnObject();
+            _spawnTime = Random.Range(_min, _max);
+            _currentTime = _min;
+        }
+    }
+
+    void SpawnObject()
+    {
+        _final = Camera.main.ViewportToWorldPoint(new Vector3(Random.Range(0f, 1f), 1, (10)));
+        Instantiate(meteor, _final, Quaternion.identity);
+    }
+}
