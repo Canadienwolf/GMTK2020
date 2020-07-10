cf96e982922eba40eba93e38d634454765cd7a2e
Hafrass <sindreva3@gmail.com>
2020-07-10 23:56:25 +0200

    Simple rocket ship working

---
+++ GMTK2020/Assets/_Scripts/PlanetGravity.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlanetGravity : MonoBehaviour
+{
+    public float mass;
+    public float speed;
+    public Vector3 startVel;
+
+    PlanetGravity[] planets;
+    [HideInInspector] public Vector3 currentVel;
+
+    private void Awake()
+    {
+        planets = FindObjectsOfType<PlanetGravity>();
+
+        currentVel = startVel;
+    }
+
+    private void FixedUpdate()
+    {
+        for (int i = 0; i < planets.Length; i++)
+        {
+            if (planets[i] != this)
+            {
+                float sqaureDist = (planets[i].GetComponent<Rigidbody>().position - GetComponent<Rigidbody>().position).sqrMagnitude;
+                Vector3 forceDir = (planets[i].GetComponent<Rigidbody>().position - GetComponent<Rigidbody>().position).normalized;
+                Vector3 force = forceDir * planets[i].mass;
+                Vector3 acceleration = force / mass;
+                currentVel += acceleration * Time.fixedDeltaTime * speed / 10;
+            }
+        }
+
+        GetComponent<Rigidbody>().position += currentVel * Time.fixedDeltaTime * speed / 10;
+    }
+
+    public Vector3 ClosestPlanetPos()
+    {
+        Vector3 closestV3 = new Vector3();
+        float closestDist = 0;
+
+        for (int i = 0; i < planets.Length; i++)
+        {
+            if (planets[i] != this)
+            {
+                if (Vector3.Distance(planets[i].transform.position, transform.position) < closestDist || closestDist == 0)
+                {
+                    closestDist = Vector3.Distance(planets[i].transform.position, transform.position);
+                    closestV3 = planets[i].transform.position;
+                }
+            }
+        }
+
+        return closestV3;
+    }
+}
---
+++ GMTK2020/Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlanetGravity))]
+public class PlayerController : MonoBehaviour
+{
+    public float rotateSpeed = 10f;
+    public float thrustPower = 10f;
+    public Camera cam;
+    public float camSpeed = 30f;
+    public float camOffset = 10;
+
+    private PlanetGravity pg;
+
+    private void Start()
+    {
+        pg = GetComponent<PlanetGravity>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.forward, Input.GetAxis("Horizontal") * -rotateSpeed * Time.deltaTime, Space.World);
+        if (Input.GetKey("space"))
+        {
+            Vector3 forceDir = transform.TransformDirection(Vector3.up);
+            pg.currentVel += forceDir * thrustPower * Time.deltaTime;
+        }
+
+        cam.transform.position = Vector3.MoveTowards(cam.transform.position, pg.ClosestPlanetPos() + new Vector3(0, 0, -camOffset), Time.deltaTime * camSpeed);
+    }
+}
