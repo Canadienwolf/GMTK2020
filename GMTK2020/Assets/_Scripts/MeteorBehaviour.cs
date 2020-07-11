ba4069b3ecb2d07a0474369923fe75caac586ccd
Frank <canadienwolf@gmail.com>
2020-07-11 17:19:26 +0200

    1

--- GMTK2020/Assets/_Scripts/MeteorBehaviour.cs
+++ GMTK2020/Assets/_Scripts/MeteorBehaviour.cs
@@ -1,18 +1,31 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MeteorBehaviour : MonoBehaviour
 {
+    public float speed;
+
+    private GameObject player;
+    private float step;
+    private Vector3 lastPlayerDir;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+       player = GameObject.FindWithTag("Player");
+       lastPlayerDir = player.transform.position - transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(lastPlayerDir * Time.deltaTime * speed);
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        Destroy(this);
     }
 }
