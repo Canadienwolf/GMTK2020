0f997f445d7a54e6c3c617ea7f22f4a89011256a
Hafrass <sindreva3@gmail.com>
2020-07-11 19:06:18 +0200

    Level generator

---
+++ GMTK2020/Assets/_Scripts/LevelGenerator.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+    public GameObject[] planets;
+    public float difficultyInterval = 10;
+
+    private int difficultyCounter = 3;
+    private int intervalCounter;
+    Vector3 spawnPos = new Vector3();
+    GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+
+        SpawnPlanet();
+        SpawnPlanet();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(player.transform.position, spawnPos) < 150)
+        {
+            SpawnPlanet();
+        }
+    }
+
+    void SpawnPlanet()
+    {
+        GameObject obj = Instantiate(planets[Random.Range(0, difficultyCounter)], spawnPos, Quaternion.identity);
+        if (intervalCounter == difficultyInterval)
+        {
+            difficultyCounter++;
+            intervalCounter = 0;
+        }
+        else
+        {
+            intervalCounter++;
+        }
+
+        spawnPos.x = 0;
+        spawnPos += new Vector3(Random.Range(-100, 100), Random.Range(70, 90), 0);
+    }
+}
