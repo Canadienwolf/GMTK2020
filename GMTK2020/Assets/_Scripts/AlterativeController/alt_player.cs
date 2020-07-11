30b0e87d29552d70e0affb9b2b4aca81764314e8
Hafrass <sindreva3@gmail.com>
2020-07-11 15:54:57 +0200

    Planet sprites

---
+++ GMTK2020/Assets/_Scripts/AlterativeController/alt_player.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class alt_player : MonoBehaviour
+{
+    public bool gravity;
+    public float forwardSpeed = 10f;
+
+    Transform currentPlanet;
+
+    private void Update()
+    {
+        transform.Translate(Vector3.up * Time.deltaTime * forwardSpeed);
+
+        if (gravity && Input.GetKeyDown("space"))
+        {
+            gravity = false;
+        }
+
+        if (gravity && currentPlanet != null)
+        {
+            print(transform.position - currentPlanet.position);
+            gravity = false;
+            //transform.right = Vector3.MoveTowards(transform.right, transform.position - currentPlanet.position + new Vector3(0, 0, 10), Time.deltaTime * 100);
+        }
+    }
+
+    public void CurrentPlanet(Transform planet)
+    {
+        currentPlanet = planet;
+        gravity = true;
+    }
+}
